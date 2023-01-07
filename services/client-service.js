41fe0229b690f4f8d8d94e1ed9d1ecd07dcb3c1b
JorshuaEs <estradajorshua16@gmail.com>
2023-01-07 16:44:09 -0600

    Conectando pagina de edicion al servidor

--- services/client-service.js
+++ services/client-service.js
@@ -17,12 +17,17 @@ const eliminarCliente = (id) =>{
         method: 'DELETE',
     });
 };
+
+const detalleCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) => respuesta.json());
+}
     
 
 export const clientServices = {
     listaClientes,
     crearCliente,
     eliminarCliente,
+    detalleCliente,
 };
 
 
